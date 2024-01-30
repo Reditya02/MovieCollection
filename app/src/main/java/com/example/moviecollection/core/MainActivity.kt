b35494e6b987c20b49fa7c484f586129d1c5361e
Reditya02 <putureditya@gmail.com>
2024-01-30 22:55:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/moviecollection/core/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.moviecollection.core
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.moviecollection.core.theme.MovieCollectionTheme
+import com.example.moviecollection.ui.HomeContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MovieCollectionTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeContent()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MovieCollectionTheme {
+        Greeting("Android")
+    }
+}
