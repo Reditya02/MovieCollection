b2331798a14309329018f32725a42fcacee7d2be
mandiri <putureditya@gmail.com>
2024-02-01 18:24:55 +0700

    day 2 : implement infinite scroll for list movie

--- app/src/main/java/com/example/moviecollection/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/moviecollection/data/local/AppDatabase.kt
@@ -4,13 +4,15 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.moviecollection.data.response.Genres
 import com.example.moviecollection.domain.model.MovieModel
+import com.example.moviecollection.domain.model.RemoteKeys
 
 @Database(
-    entities = [Genres::class, MovieModel::class],
+    entities = [Genres::class, MovieModel::class, RemoteKeys::class],
     version = 1,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun genreDao(): GenreDao
     abstract fun movieDao(): MovieDao
+    abstract fun remoteKeysDao(): RemoteKeysDao
 }
--- app/src/main/java/com/example/moviecollection/data/remotemediator/MovieRemoteMediator.kt
+++ app/src/main/java/com/example/moviecollection/data/remotemediator/MovieRemoteMediator.kt
@@ -7,6 +7,7 @@ import com.example.moviecollection.data.datasources.LocalDataSources
 import com.example.moviecollection.data.datasources.RemoteDataSources
 import com.example.moviecollection.data.local.AppDatabase
 import com.example.moviecollection.domain.model.MovieModel
+import com.example.moviecollection.domain.model.RemoteKeys
 
 class MovieRemoteMediator(
     private val remoteDataSources: RemoteDataSources,
@@ -22,20 +23,65 @@ class MovieRemoteMediator(
         loadType: LoadType,
         state: PagingState<Int, MovieModel>
     ): MediatorResult {
-        val initialPage = 1
+        val page = when (loadType) {
+            LoadType.REFRESH ->{
+                val remoteKeys = getRemoteKeyClosestToCurrentPosition(state)
+                remoteKeys?.nextKey?.minus(1) ?: 1
+            }
+            LoadType.PREPEND -> {
+                val remoteKeys = getRemoteKeyForFirstItem(state)
+                val prevKey = remoteKeys?.prevKey
+                    ?: return MediatorResult.Success(endOfPaginationReached = remoteKeys != null)
+                prevKey
+            }
+            LoadType.APPEND -> {
+                val remoteKeys = getRemoteKeyForLastItem(state)
+                val nextKey = remoteKeys?.nextKey
+                    ?: return MediatorResult.Success(endOfPaginationReached = remoteKeys != null)
+                nextKey
+            }
+        }
 
         return try {
             val response = remoteDataSources.getListMovieByGenre(
-                genre = genre, page = initialPage
+                genre = genre, page = page
             )
             val endOfPagination = response.isEmpty()
+
             if (loadType == LoadType.REFRESH) {
+                database.remoteKeysDao().deleteRemoteKeys()
                 database.movieDao().deleteByGenre(genre)
             }
+
+            val prevKey = if (page == 1) null else page - 1
+            val nextKey = if (endOfPagination) null else page + 1
+            val keys = response.map {
+                RemoteKeys(id = it.id.toString(), prevKey = prevKey, nextKey = nextKey)
+            }
+            database.remoteKeysDao().insertAll(keys)
+
             database.movieDao().insert(response)
             MediatorResult.Success(endOfPagination)
         } catch (exception: Exception) {
             MediatorResult.Error(exception)
+        }
+    }
+
+    private suspend fun getRemoteKeyForLastItem(state: PagingState<Int, MovieModel>): RemoteKeys? {
+        return state.pages.lastOrNull { it.data.isNotEmpty() }?.data?.lastOrNull()?.let { data ->
+            database.remoteKeysDao().getRemoteKeysId(data.id.toString())
+        }
+    }
+    private suspend fun getRemoteKeyForFirstItem(state: PagingState<Int, MovieModel>): RemoteKeys? {
+        return state.pages.firstOrNull { it.data.isNotEmpty() }?.data?.firstOrNull()?.let { data ->
+            database.remoteKeysDao().getRemoteKeysId(data.id.toString())
+        }
+    }
+    private suspend fun getRemoteKeyClosestToCurrentPosition(state: PagingState<Int, MovieModel>): RemoteKeys? {
+        return state.anchorPosition?.let { position ->
+            state.closestItemToPosition(position)?.id?.let { id ->
+                database.remoteKeysDao().getRemoteKeysId(id.toString())
+            }
         }
     }
 }
