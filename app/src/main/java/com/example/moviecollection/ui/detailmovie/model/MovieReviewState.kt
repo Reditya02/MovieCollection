1b947e2fdf4a599e2115bc4ddeb0b76e059c6266
mandiri <putureditya@gmail.com>
2024-02-04 17:47:17 +0700

    day 3.1 : code cleanup

--- app/src/main/java/com/example/moviecollection/ui/detailmovie/model/MovieReviewState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.moviecollection.ui.detailmovie.model
-
-import com.example.moviecollection.data.response.MovieReviewResultsItem
-
-data class MovieReviewState(
-    val isLoading: Boolean = true,
-    val errorMessage: String = "",
-    val result: List<MovieReviewResultsItem> = emptyList()
-)
